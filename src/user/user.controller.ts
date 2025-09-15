e383eb21bdd12d1330b2cbf5fc09711bd7dce7d2
Diego Reis <diegoreso@me.com>
2025-09-15 17:17:37 -0300

    feat: created database connection, user controler and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Inject, NotFoundException, Param, Post } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    @Inject()
+    private readonly userService: UserService;
+
+
+    @Post('signup')
+    async singupUser(
+        @Body() userData: Prisma.UserCreateInput,
+    ): Promise<User> {
+        return this.userService.createUser(userData);
+    }
+
+    @Get(':id')
+    async getUserById(@Param('id') id: Number): Promise<User> {
+        const user = await this.userService.getUserById({ id: Number(id) });
+        if(!user){
+            throw new NotFoundException(`User with id ${id} not found!`);
+        }
+
+        return user;
+    }
+
+}
