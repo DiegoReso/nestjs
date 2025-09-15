09b651e14239e71daa88b6364c722ebeb52ba72a
Diego Reis <diegoreso@me.com>
2025-09-15 19:08:09 -0300

    feat: create user auth with jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Inject, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { sign } from 'crypto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @Inject()
+    private readonly authService: AuthService;
+
+
+    @HttpCode(HttpStatus.OK)
+    @Post('signin')
+    signin(@Body() body: Prisma.UserCreateInput){
+        return this.authService.singin(body);
+    }
+
+
+}
